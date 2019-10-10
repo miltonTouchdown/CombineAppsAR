8db12703ecb8a94da1c794b02f78fcf8446b6e40
Milton Pulgar <miltonmja@hotmail.com>
2019-10-10 15:29:49 -0300

    + Updated plugin NativeShare + Update script to save screenshot and share

--- Assets/Scripts/SaveScreenShoot.cs
+++ Assets/Scripts/SaveScreenShoot.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -19,34 +20,47 @@ public class SaveScreenShoot : MonoBehaviour
     {
         Debug.Log("C_TakePhoto");
         isProcessing = true;
+        //yield return new WaitForEndOfFrame();
+
+
+        //string file = "Screen_" + GetTimestamp(DateTime.Now) + ".png";
+        //ScreenCapture.CaptureScreenshot(file);
+
+        //file = Application.persistentDataPath + "/"+ file;
+
+        //Debug.Log("file: " + file);
+        //int count = 0;
+        //while (!System.IO.File.Exists(file) || count > 4)
+        //{
+        //    yield return null;
+        //    Debug.Log(System.IO.File.Exists(file));
+        //}
+
+        //if (count > 4)
+        //{
+        //    Debug.Log("Error too much time taking photo");
+        //}
+        //else
+        //{
+        //    Debug.Log("Call native share");
+        //    NativeShare nativeShare = new NativeShare();
+        //    nativeShare.AddFile(file);
+        //    nativeShare.Share();
+        //}
+
         yield return new WaitForEndOfFrame();
 
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
 
-        string file = "Screen_" + GetTimestamp(DateTime.Now) + ".png";
-        ScreenCapture.CaptureScreenshot(file);
+        string filePath = Path.Combine(Application.temporaryCachePath, "Trophies.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
 
-        file = Application.persistentDataPath + "/"+ file;
+        // To avoid memory leaks
+        Destroy(ss);
 
-        Debug.Log("file: " + file);
-        int count = 0;
-        while (!System.IO.File.Exists(file) || count > 4)
-        {
-            yield return null;
-            Debug.Log(System.IO.File.Exists(file));
-        }
-
-        if (count > 4)
-        {
-            Debug.Log("Error too much time taking photo");
-        }
-        else
-        {
-            Debug.Log("Call native share");
-            NativeShare nativeShare = new NativeShare();
-            nativeShare.AddFile(file);
-            nativeShare.Share();
-        }
-        
+        new NativeShare().AddFile(filePath).Share();
 
         isProcessing = false;
     }
